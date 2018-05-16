ef045b12bb1e22f51117405bc4968792e5323ae2
Saulo Carvalho <saulo.carv@hotmail.com>
2018-05-16 20:47:08 -0300

    Criando uma classe pessoa

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,10 +3,26 @@
 
 namespace ConsoleApp
 {
+
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Pessoa aluno;
+            aluno = new Pessoa();
+            aluno.nome = "Andre";
+            aluno.idade = 20;
+
+            Console.WriteLine(aluno.nome);
+
+            Console.ReadKey();
+            
         }
     }
 }
